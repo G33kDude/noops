699e65da15bb2f8c7cf9845b11360f26eb70edd5
Jesse Horne <j.horne2796@gmail.com>
2015-01-16 12:14:29 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+Noops
+=====
+
+#### PLEASE NOTE
+##### This is probably the worst unfinished implementation you will ever see
+
+##### Noops is and always will be a general purpose programming language without operators ({, }, +, -, etc). I'm not starting out with a detailed specification. I want the Noops community to turn Noops it into whatever the majority wants it to be. So, grab some coffee, turn on some music, and don't be a stranger.
+
+## Current Requirements
+Lua 5.* (Note: it should with 5.1, 5.2, and 5.3. If it doesn't, create an issue)
+
+## Communication
+##### If you don't know about IRC, it's time to learn. We use it for ALL developer communication. We need your help, so please join us. And no, it's not on (un)Freenode.
+###### If you have trouble connecting, email me (jhorne at hashbang dot sh)
+```
+irc.hashbang.sh/6697 #!noops
+```
+
+## How to Run
+```
+lua noops.lua examples/hello_world.noops
+```
+
+#### Please note, documentation on this language is coming soon. All of your questions will be answered there. I'm sure you have some.
+
+##### But is it turing complete? Not yet.
+
+#### Example Scripts That Work
+```
+hello_world.noops
+variable_test.noops
+empty.noops
+```
---
+++ noops.lua
@@ -0,0 +1,94 @@
+-- Forgive me for what you're about to read
+
+local warnings = 0
+local errors = 0
+
+local code = nil
+
+-- execs holds lua executable statements created from noops code
+local execs = {}
+
+function string:split(sep)
+  local sep, fields = sep or ":", {}
+  local pattern = string.format("([^%s]+)", sep)
+  self:gsub(pattern, function(c) fields[#fields+1] = c end)
+  return fields
+end
+
+local warning = function(text)
+  warnings = warnings + 1
+  print("WARNING[" .. warnings .. "]: " .. text .. "!")
+end
+
+local error = function(text)
+  errors = errors + 1
+  print("ERROR[" .. errors .. "]: " .. text .. "!")
+  os.exit()
+end
+
+local filename = arg[1]
+local args = (function() table.remove(arg, 1); return arg end)()
+
+-- Initial noops.lua warnings and errors
+if not filename then
+  error("The filename argument is missing")
+else
+  code = io.open(filename, "r")
+  if not code then
+    error("There is no code in " .. filename)
+  else
+    code = code:read("*all")
+    if code == "" then
+      error("There is no code in " .. filename)
+    end
+  end
+end
+
+if #args == 0 then
+  warning("No arguments to this script, I hope that was intentional")
+end
+
+-- Right here, in the future, go through and error if any operators are found
+-- Because as it stands, you can still use them, because I'm lazy
+
+local code_by_newlines = code:split("\n")
+
+-- Highest parsing scope
+
+for i,v in ipairs(code_by_newlines) do
+  local line = v:split(" ")
+  if line[1] == "print" then
+    local str = "print("
+    local code = line
+    table.remove(code, 1)
+
+    for x,y in ipairs(code) do
+      if _G[y] then
+        if x < #code then
+          local bleh = _G[y] .. " .. \" \""
+          str = str .. bleh .. " .. "
+        else
+          str = str .. _G[y]
+        end
+      else
+        if x < #code then
+          str = str .. "\"" .. y .. " \" .. "
+        else
+          str = str .. "\"" .. y .. "\""
+        end
+      end
+    end
+    str = str .. ")"
+    table.insert(execs, str)
+  elseif line[1] == "var" then
+    -- local code = string.gsub(v, "var ", "")
+    -- code = string.gsub(code, "is", "=")
+    -- code = string.gsub(code, line[2], "_G['" .. line[2] .. "']")
+    -- table.insert(execs, code)
+    -- print(code)
+    _G[line[2]] = line[4]
+  end
+end
+
+-- Time to run the code
+loadstring(table.concat(execs, ";"))()
